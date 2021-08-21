46a052a22521b570a4a71242d8ada667291a6ab4
kirsergienko <kiril.street3@gmail.com>
2021-08-21 20:32:56 +0300

    bag

--- Recognizer/Form1(methods).cs
+++ Recognizer/Form1(methods).cs
@@ -28,7 +28,7 @@ namespace Recognizer
                 var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
                 while (check)
                 {
-                    await FromDevice(speechConfig,audioConfig);
+                    await FromDevice(speechConfig, audioConfig);
                 }
             }
             else
@@ -43,7 +43,7 @@ namespace Recognizer
                 while (check)
                 {
                     await FromDevice(speechConfig, audioConfig);
-                    
+
                 }
             }
         }
@@ -67,7 +67,11 @@ namespace Recognizer
                 {
                     Action action = new Action(async () =>
                     {
-                        history.Add(await Translator.Translate(Resultator.GetResult(e), settings.OutputLanguage));
+                        string res = await Translator.Translate(Resultator.GetResult(e), settings.OutputLanguage);
+                        if (!String.IsNullOrWhiteSpace(res))
+                        {
+                            history.Add(res);
+                        }
                     });
                     Invoke(action);
                 };
@@ -81,100 +85,100 @@ namespace Recognizer
 
                 await recognizer.StopContinuousRecognitionAsync();
             }
-            
+
         }
 
-       
+
 
         private string SetLanguage(string language)
         {
-                switch (language)
-                {
-                    case "Arabic":
-                        return "ar-SA";
-                    case "Bulgarian":
-                        return "bg-BG";
-                    case "Catalan":
-                        return "ca-ES";
-                    case "Chinese Simplified":
-                        return "zh-Hans";
-                    case "Chinese Traditional":
-                        return "zh-Hant";
-                    case "Croatian":
-                        return "ru-RU";
-                    case "Czech":
-                        return "cs-CZ";
-                    case "Danish":
-                        return "da-DK";
-                    case "Dutch":
-                        return "nl-NL";
-                    case "English":
-                        return "en-US";
-                    case "Estonian":
-                        return "et-EE";
-                    case "Finnish":
-                        return "fi-FI";
-                    case "French":
-                        return "fr-FR";
-                    case "German":
-                        return "de-DE";
-                    case "Greek":
-                        return "el-GR";
-                    case "Gujarati":
-                        return "gu-IN";
-                    case "Hebrew":
-                        return "he-IL";
-                    case "Hindi":
-                        return "hi-IN";
-                    case "Hungarian":
-                        return "hu-HU";
-                    case "Indonesian":
-                        return "id-ID";
-                    case "Irish":
-                        return "ga-IE";
-                    case "Italian":
-                        return "it-IT";
-                    case "Japanese":
-                        return "ja-JP";
-                    case "Korean":
-                        return "ko-KR";
-                    case "Latvian":
-                        return "lv-LV";
-                    case "Lithuanian":
-                        return "it-LT";
-                    case "Malay":
-                        return "ms-MY";
-                    case "Maltese":
-                        return "mt-MT";
-                    case "Norwegain":
-                        return "nb-NO";
-                    case "Polish":
-                        return "pl-PL";
-                    case "Portuguese(Brazil)":
-                        return "pt-BR";
-                    case "Portuguese(Portugal)":
-                        return "pt-PT";
-                    case "Romanian":
-                        return "ro-RO";
-                    case "Russian":
-                        return "ru-RU";
-                    case "Slovak":
-                        return "sk-SK";
-                    case "Slovenian":
-                        return "sl-SL";
-                    case "Spanish":
-                        return "es-ES";
-                    case "Swedish":
-                        return "sv-SE";
-                    case "Thai":
-                        return "th-TH";
-                    case "Turkish":
-                        return "tr-TR";
-                    case "Vietnamese":
-                        return "vi-VN";
-                    default:
-                        return "en-US";
-                }
+            switch (language)
+            {
+                case "Arabic":
+                    return "ar-SA";
+                case "Bulgarian":
+                    return "bg-BG";
+                case "Catalan":
+                    return "ca-ES";
+                case "Chinese Simplified":
+                    return "zh-Hans";
+                case "Chinese Traditional":
+                    return "zh-Hant";
+                case "Croatian":
+                    return "ru-RU";
+                case "Czech":
+                    return "cs-CZ";
+                case "Danish":
+                    return "da-DK";
+                case "Dutch":
+                    return "nl-NL";
+                case "English":
+                    return "en-US";
+                case "Estonian":
+                    return "et-EE";
+                case "Finnish":
+                    return "fi-FI";
+                case "French":
+                    return "fr-FR";
+                case "German":
+                    return "de-DE";
+                case "Greek":
+                    return "el-GR";
+                case "Gujarati":
+                    return "gu-IN";
+                case "Hebrew":
+                    return "he-IL";
+                case "Hindi":
+                    return "hi-IN";
+                case "Hungarian":
+                    return "hu-HU";
+                case "Indonesian":
+                    return "id-ID";
+                case "Irish":
+                    return "ga-IE";
+                case "Italian":
+                    return "it-IT";
+                case "Japanese":
+                    return "ja-JP";
+                case "Korean":
+                    return "ko-KR";
+                case "Latvian":
+                    return "lv-LV";
+                case "Lithuanian":
+                    return "it-LT";
+                case "Malay":
+                    return "ms-MY";
+                case "Maltese":
+                    return "mt-MT";
+                case "Norwegain":
+                    return "nb-NO";
+                case "Polish":
+                    return "pl-PL";
+                case "Portuguese(Brazil)":
+                    return "pt-BR";
+                case "Portuguese(Portugal)":
+                    return "pt-PT";
+                case "Romanian":
+                    return "ro-RO";
+                case "Russian":
+                    return "ru-RU";
+                case "Slovak":
+                    return "sk-SK";
+                case "Slovenian":
+                    return "sl-SL";
+                case "Spanish":
+                    return "es-ES";
+                case "Swedish":
+                    return "sv-SE";
+                case "Thai":
+                    return "th-TH";
+                case "Turkish":
+                    return "tr-TR";
+                case "Vietnamese":
+                    return "vi-VN";
+                default:
+                    return "en-US";
+            }
         }
     }
 }
